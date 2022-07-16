a91e64cec3d1a0360910e2d2de1c696883b8cb69
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-07-15 23:32:20 -0300

    feat: implements operations create, get and delete in card entity

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import authRouter from "./authRouter.js";
 import notesRouter from "./notesRouter.js";
 import wifiRouter from "./wifiRouter.js";
+import cardsRouter from "./cardsRouter.js";
 import credentialsRouter from "./credentialsRouter.js";
 
 const router = Router();
@@ -10,6 +11,7 @@ const router = Router();
 router.use(authRouter);
 router.use(wifiRouter);
 router.use(notesRouter);
+router.use(cardsRouter);
 router.use(credentialsRouter);
 
 
